f16955c0049209132cfa221e711947324e57bca8
yt6103.lee <yt6103.lee@partner.multicampus.com>
2024-11-12 12:35:05 +0900

    11.5

--- components/main-header.js
+++ components/main-header.js
@@ -2,22 +2,25 @@ import Link from "next/link";
 
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
+import Image from "next/image";
 
 export default function MainHeader() {
-    return <header id="main-header">
-        <Link className={classes.logo} href="/">
-            <img src={logoImg.src} alt="A plat with food on it" />
-            NextLevel Food
-        </Link>
-        <nav>
-            <ul>
-                <li>
-                    <Link href="/meals">Brows Meals</Link>
-                </li>
-                <li>
-                    <Link href="/community">Foodies Community</Link>
-                </li>
-            </ul>
-        </nav>
-    </header>
+    return (
+        <header className={classes.header}>
+            <Link className={classes.logo} href="/">
+                <Image src={logoImg} alt="A plat with food on it" priority />
+                NextLevel Food
+            </Link>
+            <nav className={classes.nav}>
+                <ul>
+                    <li>
+                        <Link href="/meals">Brows Meals</Link>
+                    </li>
+                    <li>
+                        <Link href="/community">Foodies Community</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
 }
